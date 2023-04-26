38891662ab792e32e5b3991bc6c65c2f173ea716
Your name <tahrijouteyamer@gmail.com>
2023-04-25 17:12:53 -0700

    .

--- main.h
+++ main.h
@@ -7,20 +7,22 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
+/* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
- * struct fmt - the struct operator
+ * struct fmt - Struct op
  *
- * @fmt: the format
- * @fn: the function
+ * @fmt: The format.
+ * @fn: The function associated.
  */
 struct fmt
 {
@@ -30,16 +32,17 @@ struct fmt
 
 
 /**
- * typedef struct fmt fmt_t - the struct operator
+ * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: the format
- * @fm_t: the function
+ * @fmt: The format.
+ * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
